689c088a9e52436e86120d0338fab74ef26f350a
rafarmerjr1 <robert.farmer000@gmail.com>
2022-02-06 11:54:45 -0500

    fixing XXE

--- html/db.php
+++ html/db.php
@@ -1,8 +1,11 @@
 <?php
     function OpenCon()
      {
-     //$dbhost = "localhost";
-     $dbhost = "db";
+    /*
+    DBhost must be changed depending on if you are using dockerized webapp
+    or a locally hosted httpd/mariaDB webapp */
+     $dbhost = "localhost";  //localhost for local apache/mysql httpd/mariaDB
+     //$dbhost = "db"; //db for referenced DB in docker-docker.yml
      $dbuser = "admin"; #db admin NOT webadmin
      $dbpass = "administratorPassword!!!";
      $db = "webapp";
